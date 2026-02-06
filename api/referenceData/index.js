ceade207d4503f94e39552f7794e987fdb549335
MohammedEuromonitor <mohammed.laeeq@euromonitor.com>
2026-02-06 15:16:56 +0530

    Add reference data API endpoint
    Implement reference data API to return country, industry, or category data based on query parameter.

--- api/referenceData/index.js
+++ api/referenceData/index.js
@@ -1,1 +1,61 @@
-testing
+module.exports = async function (context, req) {
+  try {
+    const type = req.query.type;
+
+    let data = [];
+
+    switch (type) {
+      case "country":
+        data = [
+          "United Kingdom",
+          "United States",
+          "India",
+          "Germany",
+          "France"
+        ];
+        break;
+
+      case "industry":
+        data = [
+          "Retail",
+          "Consumer Electronics",
+          "Healthcare",
+          "Technology",
+          "Food & Beverage"
+        ];
+        break;
+
+      case "category":
+        data = [
+          "Grocery",
+          "Apparel",
+          "Beauty & Personal Care",
+          "Home Care"
+        ];
+        break;
+
+      default:
+        context.res = {
+          status: 400,
+          body: { error: "Invalid type parameter" }
+        };
+        return;
+    }
+
+    context.res = {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: data
+    };
+
+  } catch (error) {
+    context.log.error("Error in referenceData API:", error);
+
+    context.res = {
+      status: 500,
+      body: { error: "Internal server error" }
+    };
+  }
+};
